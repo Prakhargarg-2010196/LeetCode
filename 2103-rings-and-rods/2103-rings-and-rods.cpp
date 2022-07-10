a2b4c3fbe19dce9f0f34712bfcdafa78f47ca244
Prakhargarg <23prakhargarg2002@gmail.com>
2022-07-11 03:31:55 +0530

    Time: 2 ms (56.55%), Space: 6.5 MB (22.76%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.cpp
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -1,26 +1,47 @@
-#include<vector>
+#include<unordered_map>
+#include<set>
 class Solution {
 public:
     int countPoints(string rings) {
-        int r[10]{0}, b[10]{0},g[10]{0};
+        unordered_map<char,set<char>>ringMap;
         int count=0;
-        for(int i=0;i<rings.length();i+=2)
-        {   
-            int rod=rings[i+1]-'0';
-            if(rings[i]=='R')
-                r[rod]++;
-            else if(rings[i]=='G')
-                g[rod]++;
-            else if(rings[i]=='B')
-                b[rod]++;
-            
+        for(int i=0,j=1;i<rings.size();i+=2,j+=2)
+        {
+            ringMap[rings[j]].insert(rings[i]);
         }
-        for(int i=0;i<10;i++)
-        {   
-            if(r[i]&&g[i]&&b[i])
+        for(auto it:ringMap)
+        {
+            if(it.second.size()==3)
+            {
                 count++;
+            }
         }
-        
         return count;
     }
 };
+// #include<vector>
+// class Solution {
+// public:
+//     int countPoints(string rings) {
+//         int r[10]{0}, b[10]{0},g[10]{0};
+//         int count=0;
+//         for(int i=0;i<rings.length();i+=2)
+//         {   
+//             int rod=rings[i+1]-'0';
+//             if(rings[i]=='R')
+//                 r[rod]++;
+//             else if(rings[i]=='G')
+//                 g[rod]++;
+//             else if(rings[i]=='B')
+//                 b[rod]++;
+            
+//         }
+//         for(int i=0;i<10;i++)
+//         {   
+//             if(r[i]&&g[i]&&b[i])
+//                 count++;
+//         }
+        
+//         return count;
+//     }
+// };
