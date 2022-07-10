879e6c80542f8875ec5f1cd9ae58fa996a8b29fb
Prakhargarg <23prakhargarg2002@gmail.com>
2022-07-10 18:40:52 +0530

    Time: 3 ms (60.77%), Space: 6 MB (55.15%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -6,19 +6,10 @@ public:
     bool digitCount(string num) {
         unordered_map<char,int> mpp;
         for(auto& i:num)
-        {   
-            mpp[i]++;
-          
-        }
-        
+          mpp[i]++;
         for(int i=0;i<num.length();i++)
-        {
             if(mpp[i+'0']!=num[i]-'0')
                 return false;
-            
-            
-
-        }
         // for(auto& it:mpp)
         // {
         //     cout<<it.first<<" "<<it.second<<endl;
