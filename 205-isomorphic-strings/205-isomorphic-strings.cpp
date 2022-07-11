fcc360ef78d7167275f96dc81f1131c8e4e66ef9
Prakhargarg <23prakhargarg2002@gmail.com>
2022-07-11 23:01:21 +0530

    Time: 17 ms (22.25%), Space: 7.1 MB (19.44%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -8,7 +8,13 @@ public:
             map_st[s[i]]=t[i];
             map_ts[t[i]]=s[i];
         }
-        
+/* In this question we need to check for one to one relationship between characters of two strings so we need two maps  
+
+step 1: Create two maps:
+            i.) first map: for first string characters to be mapped to second string characters.
+            ii.) second map :for second string characters to be mapped to first string characters.
+step 2: Check if the value of one map to the other map if both map to each other than the strings are isomporphic otherwise not.
+*/ 
         for(int i=0;i<lengthOfString;i++)
         {
             if(!(map_st[s[i]]==t[i]&&map_ts[t[i]]==s[i]))
