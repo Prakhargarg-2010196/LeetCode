4a7c636e568d2e7ab82f6db23ad0b3ad0ad11dbd
Prakhargarg <23prakhargarg2002@gmail.com>
2022-07-22 02:53:14 +0530

    Time: 3 ms (46.38%), Space: 6.4 MB (27.01%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,12 +1,12 @@
+#include <algorithm>
 class Solution {
 public:
-    
-    int maxDepth(string s) {
-     stack<char> vps;
-// ignore numbers and symbols
-// take one counter variable(count) for opening parenthesis
-// increase the counter if opening "(" is encountered while reduce it if ')' is encountered
-// at last find the max of the previous counter value using another variable maxCount;
+    int withoutStackApproach(string s)
+    {
+        // ignore numbers and symbols
+        // take one counter variable(count) for opening parenthesis
+        // increase the counter if opening "(" is encountered while reduce it if ')' is encountered
+        // at last find the max of the previous counter value using another variable maxCount;
      int maxCount=0,count=0;
      for(auto it:s)
      {  
@@ -19,6 +19,28 @@ public:
         
      }
         return maxCount;
+    }
+    int stackApproach(string s)
+    {   stack<char> stk;
+        int maxOpeningParenthesis=0;
+        //As the goal is to calculate number of opening parenthesis.
+        for(auto character:s)
+        {    if(character=='(')
+                stk.push(character);
+             else if(character==')')
+             {   int size=stk.size();// as the max function requires same type variables;   
+                 maxOpeningParenthesis=max(maxOpeningParenthesis,size);
+                 //Comparing with stk.size() because stack size reduces after every encounter of closing brackets and increases after each encounter of opening one.
+                 stk.pop();
+                 // pop as soon as the closing bracket is encontered this reduces the stack size indicating a decrease in opening brackets.
+              
+              }  
+         }
+     return maxOpeningParenthesis;
+    }
+    int maxDepth(string s) {
+     
+    return stackApproach(s);
         
     }
 };
