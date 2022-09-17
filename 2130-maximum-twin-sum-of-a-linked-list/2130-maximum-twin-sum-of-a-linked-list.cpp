0115b777f23ed03943dfafacc9e6108d0771c7db
Prakhargarg <23prakhargarg2002@gmail.com>
2022-09-17 11:32:39 +0530

    Time: 1268 ms (5.04%), Space: 116.7 MB (58.77%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// Related Problems 
+class Solution {
+public:
+//     return middle of the linked list 
+    ListNode* middleNode(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL&&fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+//     return the reverse of the linked list using iteration method
+    ListNode* reverseList(ListNode* head){
+        ListNode* previous=NULL;
+        ListNode* current=head;
+        while(current!=NULL){
+            ListNode* nextNode=current->next;
+            current->next=previous;
+            previous=current;
+            current=nextNode;
+        }
+        return previous;
+    }
+    int pairSum(ListNode* head) {
+//      First of the splitted list
+        ListNode* firstHalf=head;
+//         Middle of the list
+        ListNode* middle=middleNode(head);
+//         Second half of the splitted list
+        ListNode* secondHalf=reverseList(middle);
+        int maxSum=0;
+        while(firstHalf!=NULL&&secondHalf!=NULL){
+                maxSum=(firstHalf->val+secondHalf->val)>maxSum?firstHalf->val+secondHalf->val:maxSum;
+            cout<<maxSum<<endl;
+            firstHalf=firstHalf->next;
+            secondHalf=secondHalf->next;
+                
+            
+            
+        }   
+        return maxSum;
+    }
+};
