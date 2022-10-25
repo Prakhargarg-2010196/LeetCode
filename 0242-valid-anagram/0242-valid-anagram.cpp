ffbdb74d34f340606339fd8da1cd2cd23afbefe3
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-10-26 02:19:15 +0530

    Time: 12 ms (79.41%), Space: 7.2 MB (82.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         //Noob Approach
-        vector<int> countS(26,0);
-        vector<int> countT(26,0);
+        array<int,26> countS;//std::array
+        countS.fill(0);
+        array<int,26> countT;//std::array
+        countT.fill(0);
         for(int i=0;i<s.size();i++)
             countS[s[i]-97]++;
         for(int j=0;j<t.size();j++)
