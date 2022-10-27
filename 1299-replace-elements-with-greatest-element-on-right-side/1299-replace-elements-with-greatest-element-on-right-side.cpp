dd4fff4253c143b27b06871202f302993a9b3639
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-10-27 17:13:08 +0530

    Time: 42 ms (41.73%), Space: 14.6 MB (24.24%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        // without exchange function approach 
-        int mx=-1;
-        for(int i=arr.size()-1;i>=0;i--){
-            // storing the value first
-           int temp=arr[i];
-                 arr[i]=mx;
-            mx=max(mx,temp);
+        // WITHOUT EXCHANGE FUNCTION 
+        // int mx=-1;
+        // for(int i=arr.size()-1;i>=0;i--){
+        //     // storing the value first
+        //    int temp=arr[i];
+        //     //storing the current max value to the right in the present element 
+        //          arr[i]=mx;
+        //     // now find the max element to right.
+        //     mx=max(mx,temp);
+        // }
+        // return arr;
+        
+        //WITH EXCHANGE FUNCTION
+        for(int it=arr.size()-1, mx=-1;it>=0;it--){
+            
+            mx=max(mx,exchange(arr[it],mx));
+            //expands as follows
+            // exchange(arr[it],mx);
+            // max(mx,arr[it]);
         }
-        return arr;
-           
+           return arr;
     }
 };
