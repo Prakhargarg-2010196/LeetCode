7872bb1ca24250f6f29c0a3ce4d1ab548ff016a8
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-10-30 04:29:30 +0530

    Time: 145 ms (11.13%), Space: 26.2 MB (12.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -5,14 +5,31 @@ public:
         unordered_map<string,vector<string>> mp;
         for(auto& it:strs){
             string t=it;
-            sort(t.begin(),t.end());//sorting the key 
-            mp[t].push_back(it);//storing the value with same keys together
+            // sort(t.begin(),t.end());//sorting the key 
+            mp[sortString(t)].push_back(it);//storing the value with same keys together
         }
+        // saving the results in the anagrams vector
         vector<vector<string>> anagrams;
         for(auto it:mp){
                 anagrams.push_back(it.second);
         }
         
         return anagrams;
+        // SECOND APPROACH
+        
+        
+    }
+    private: 
+    string sortString(string s){
+        vector<int> countChar(26,0);
+        for(auto it:s)
+            countChar[it-'a']++;//it-'a' will subtract the ascii value of both and give the ascii value in return;
+        string str;
+        for(auto it:countChar)
+        { str+=it+'a';
+          
+         }
+        cout<<str<< " ";
+        return str;
     }
 };
