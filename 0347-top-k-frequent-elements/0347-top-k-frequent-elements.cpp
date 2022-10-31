107fb3bed8e548af874efdf81c2156d4ea125334
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-01 01:55:18 +0530

    Time: 31 ms (59.49%), Space: 13.7 MB (44.90%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,28 +1,55 @@
 class Solution {
-    public:
-    static bool pairCompare(pair<int,int> &p1,pair<int,int> &p2){
-        return p1.second>p2.second;
-    } 
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>count;
-        // made an unordered map for frequency.
+//     public:
+//     static bool pairCompare(pair<int,int> &p1,pair<int,int> &p2){
+//         return p1.second>p2.second;
+//     } 
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int>count; // O(n) space
+//         // made an unordered map for frequency.
+//         for(auto& it:nums){  
+//             count[it]++;     
+//         }
+//         vector<pair<int,int>>kFrequentPairs; //O(n) space
+//         for(auto it:count){
+//             kFrequentPairs.push_back(make_pair(it.first,it.second));
+//         }
+//         // sorted pairs using comparator function        
+//         sort(kFrequentPairs.begin(),kFrequentPairs.end(),pairCompare);
+      
+//         cout<<endl;
+//         vector<int>result;
+//         for(int i=0;i<k;i++){
+//             result.push_back(kFrequentPairs[i].first);
+//         }
+            
+//         return result;
+//     }
+ public:
+    vector<int> topKFrequent(vector<int>& nums, int k){
+        unordered_map<int,int> count;
         for(auto& it:nums){
             count[it]++;
         }
-        vector<pair<int,int>>kFrequentPairs;
-        for(auto it:count){
-            kFrequentPairs.push_back(make_pair(it.first,it.second));
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>maxFrequency;
+        for(auto&it:count){
+            maxFrequency.push(make_pair(it.second,it.first));
+            if(maxFrequency.size()>k){
+                maxFrequency.pop();
+            }
         }
-        // sorted pairs using comparator function        
-        sort(kFrequentPairs.begin(),kFrequentPairs.end(),pairCompare);
-      
-        cout<<endl;
-        vector<int>result;
-        for(int i=0;i<k;i++){
-            result.push_back(kFrequentPairs[i].first);
+        vector<int> result;
+        while(!maxFrequency.empty()){
+            result.push_back(maxFrequency.top().second);            
+            maxFrequency.pop();
         }
-            
         return result;
+        
+        
+        
+        
+        
+        
+        
+        
     }
-
 };
