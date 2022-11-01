5b86bffbcf56e1ace7c7d0d0c30ae6446cf83f3e
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-02 01:24:16 +0530

    Time: 58 ms (39.35%), Space: 25.2 MB (20.40%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,44 @@
+class Solution {
+// Naive approach(time limit exceeds O(N2))
+// The more u practice the more great u will become |-_-|
+    // public:
+    // vector<int> productExceptSelf(vector<int>& nums) {
+    //     int i,j;
+    //     int n=nums.size();
+    //     vector<int>result(n,1);
+    //     for(i=0;i<n;i++){
+    //         for(j=0;j<n;j++){
+    //             if(i==j){
+    //                 continue;
+    //             }
+    //             result[i]=result[i]*nums[j];
+    //         }
+    //     }
+    //     return result;
+    // }
+    // second approach with
+    // T(n):O(n)
+    // S(n):O(n)// extra arrays are used to store the prefix products and suffix products.
+    public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>leftProducts(n);
+        vector<int>rightProducts(n);
+        
+        //making the first element and last element of both to be 1 as the prefix and suffixes are to leftmost and rightmost elements don't exist
+        leftProducts[0]=1;
+        rightProducts[n-1]=1;
+        //fill the arrays with respective products
+        for(int i=1;i<n;i++){
+            leftProducts[i]=leftProducts[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            rightProducts[i]=rightProducts[i+1]*nums[i+1];
+        }
+        vector<int> result(n);
+        for(int i=0;i<n;i++){
+            result[i]=leftProducts[i]*rightProducts[i];
+        }
+        return result;
+    }
+};
