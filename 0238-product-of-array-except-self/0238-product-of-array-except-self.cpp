57fde446cb9a727d8f8d037d7a66b117ddc77d76
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-02 01:50:16 +0530

    Time: 60 ms (32.73%), Space: 23.8 MB (96.59%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -19,26 +19,43 @@ class Solution {
     // second approach with
     // T(n):O(n)
     // S(n):O(n)// extra arrays are used to store the prefix products and suffix products.
+//     public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>leftProducts(n);
+//         vector<int>rightProducts(n);
+        
+//         //making the first element and last element of both to be 1 as the prefix and suffixes are to leftmost and rightmost elements don't exist
+//         leftProducts[0]=1;
+//         rightProducts[n-1]=1;
+//         //fill the arrays with respective products
+//         for(int i=1;i<n;i++){
+//             leftProducts[i]=leftProducts[i-1]*nums[i-1];
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             rightProducts[i]=rightProducts[i+1]*nums[i+1];
+//         }
+//         vector<int> result(n);
+//         for(int i=0;i<n;i++){
+//             result[i]=leftProducts[i]*rightProducts[i];
+//         }
+//         return result;
+//     }
+    //Time Complexity:O(n)
+    //Space Complexity:O(1)// no requirement of extra arrays as we can take the left product or right product as variable and take the other respective product as output array.
     public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int>leftProducts(n);
-        vector<int>rightProducts(n);
-        
-        //making the first element and last element of both to be 1 as the prefix and suffixes are to leftmost and rightmost elements don't exist
-        leftProducts[0]=1;
-        rightProducts[n-1]=1;
-        //fill the arrays with respective products
+        vector<int>leftProduct(n);
+        leftProduct[0]=1;
+        int rightProduct=1;
         for(int i=1;i<n;i++){
-            leftProducts[i]=leftProducts[i-1]*nums[i-1];
+          leftProduct[i]=leftProduct[i-1]*nums[i-1];  
         }
-        for(int i=n-2;i>=0;i--){
-            rightProducts[i]=rightProducts[i+1]*nums[i+1];
+        for(int i=n-1;i>=0;i--){
+            leftProduct[i]*=rightProduct;
+            rightProduct*=nums[i];
         }
-        vector<int> result(n);
-        for(int i=0;i<n;i++){
-            result[i]=leftProducts[i]*rightProducts[i];
-        }
-        return result;
+        return leftProduct;
     }
 };
