2bcb0161bc90701fd3d624d164079f1b3e2fd407
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-07 22:02:40 +0530

    Time: 13 ms (36.29%), Space: 7.9 MB (51.78%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+//     Time Complexity:O(m*n)
+//     m:no. of customers , n:no.of accounts each customer owns
+//     Space Complexity:O(1) // no extra space is used just variables are used.
     int maximumWealth(vector<vector<int>>& accounts) {
         int globalMaximum=0;
         for(int i=0;i<accounts.size();i++){
