2f890b85106a97898d9c4bc308b1751ea3a23065
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-07 21:58:53 +0530

    Time: 10 ms (60.42%), Space: 7.8 MB (87.41%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -3,11 +3,11 @@ public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int globalMaximum=0;
         for(int i=0;i<accounts.size();i++){
-            int localMaximum=0;
+            int localAccountsSum=0;
             for(int j=0;j<accounts[i].size();j++){
-                localMaximum=max(localMaximum,localMaximum+accounts[i][j]);
+                localAccountsSum+=accounts[i][j];
             }
-            globalMaximum=max(localMaximum,globalMaximum);
+            globalMaximum=max(localAccountsSum,globalMaximum);
         }
         return globalMaximum;
     }
