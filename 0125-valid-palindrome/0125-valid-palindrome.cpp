36f056ab3a649e9028904974cf9a9b56cbd63396
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-08 17:59:36 +0530

    Time: 18 ms (28.69%), Space: 7.3 MB (90.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string str;
-        for(auto&it:s)
-            it=('A'<=it&&it<='Z')?it+32:it;
+//         for(auto&it:s)
+//             it=('A'<=it&&it<='Z')?it+32:it;
                
-        for(auto&it:s){
-            if(it>='a'&&it<='z'||it>='0'&&it<='9'){
-                str+=it;
-            }
-        }
-        for(int i=0,j=str.size()-1;i<str.size();i++,j--){
-            if(str[i]!=str[j])
+        // for(auto&it:s){
+        //     if(it>='a'&&it<='z'||it>='0'&&it<='9'){
+        //         str+=it;
+        //     }
+        // }
+        //As the motion of the pointers is unpredictable avoid using for loop
+        int l=0,r=s.size()-1;
+        while(l<r){
+            while(l<r&&!isalnum(s[l]))
+                l++;
+            while(l<r&&!isalnum(s[r]))
+                r--;
+            if(tolower(s[l])!=tolower(s[r]))
                 return false;
-            
+            l++;
+            r--;
         }
         
     return true;    
