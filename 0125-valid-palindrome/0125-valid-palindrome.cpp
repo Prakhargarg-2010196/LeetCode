c08c25e23f7f3184879317ba07c6b2bf8b561880
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-08 18:03:36 +0530

    Time: 15 ms (42.52%), Space: 7.5 MB (55.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str;
-//         for(auto&it:s)
-//             it=('A'<=it&&it<='Z')?it+32:it;
-               
-        // for(auto&it:s){
-        //     if(it>='a'&&it<='z'||it>='0'&&it<='9'){
-        //         str+=it;
-        //     }
-        // }
-        //As the motion of the pointers is unpredictable avoid using for loop
-        int l=0,r=s.size()-1;
-        while(l<r){
-            while(l<r&&!isalnum(s[l]))
-                l++;
-            while(l<r&&!isalnum(s[r]))
-                r--;
-            if(tolower(s[l])!=tolower(s[r]))
-                return false;
-            l++;
-            r--;
+//   Method 1: Taking a space to store the string
+       string str;
+        for(auto&it:s)
+            it=('A'<=it&&it<='Z')?it+32:it;
+        for(auto&it:s){
+            if(it>='a'&&it<='z'||it>='0'&&it<='9'){
+                str+=it;
+            }
         }
+        for(int i=0,j=str.size()-1;i<str.size()/2;i++,j--)
+            if(str[i]!=str[j])
+                return false;
+        return true;
+        //Two Pointer Approach
+        //As the motion of the pointers is unpredictable avoid using for loop
+//         int l=0,r=s.size()-1;
+//         while(l<r){
+//             //ignores the spaces and other non-alphanumeric from both left and right
+//             while(l<r&&!isalnum(s[l]))
+//                 l++;
+//             while(l<r&&!isalnum(s[r]))
+//                 r--;
+//             if(tolower(s[l])!=tolower(s[r]))
+//                 return false;
+//             l++;
+//             r--;
+//         }
         
-    return true;    
+//     return true;    
     }
 };
