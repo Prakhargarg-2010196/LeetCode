7ec53477cb11ee94599706443e69ef996c8874a2
Prakhar Garg <23prakhargarg2002@gmail.com>
2022-11-18 20:27:09 +0530

    Time: 43 ms (14.98%), Space: 16.3 MB (52.46%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-     vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            ans.push_back(nums.at(nums.at(i)));
-        }
-        return ans;
+//     Time Complexity:O(n)
+//     Space Complexity:O(n);
+    //  vector<int> ans;
+    //     for(int i=0;i<nums.size();i++){
+    //         ans.push_back(nums.at(nums.at(i)));
+    //     }
+    //     return ans;
+    // 
+// To reduce the space complexity to O(1) store both numbers at same location.
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        nums[i]=nums[i]+(nums[nums[i]]%n)*n;
+        // As for some numbers the value of a can already be changed from previous values so by mod n we get previous value .
+        // a= a + (b%n)*n
+        // previous value of a = a%n;
+        // secondary value stored in a 
+        //  b=a/n;
+        
+    }
+    for(int i=0;i<n;i++){
+        nums[i]=nums[i]/n;
+    }
+        return nums;
     }
 };
