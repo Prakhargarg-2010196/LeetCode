45ac6aad9b45627d6ce27a675b8e8a3d5ad69122
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-01-14 16:53:59 +0530

    Time: 12 ms (73.59%), Space: 16.1 MB (84.21%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    bool static comp(int a,int b){
-        if(a%2<b%2){
-            return true;
-        }
-        else return false;
+//     bool static comp(int a,int b){
+//         if(a%2<b%2){  // 0 for even // 1 for odd 
+//             return true;
+//         }
+//         else return false;
         
-        // return a%2==0&&b%2!=0;// this is my solution 
+//         // return a%2==0&&b%2!=0;// this is my solution 
         
                     
-    }
+//     }
     vector<int> sortArrayByParity(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),comp);
-        
-//         int i=0;
-//         int j=1;
-//         while(i<nums.size())
-//         if(nums[i]%2==0&&nums[j]%)
-        
+        // sort(nums.begin(),nums.end(),comp);
+        int n=nums.size();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            
+            if(nums[i]%2==0)
+                i++;
+            else if(nums[j]%2!=0)
+                j--;
+            else if(nums[j]%2==0&&nums[i]%2!=0)
+            {  
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            
+        }
         return nums;
     }
 };
