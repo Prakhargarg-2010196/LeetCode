cf6661aa685a1ebd9da05e7df635bb61d80e9d27
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-01-20 17:23:04 +0530

    Time: 10 ms (91.76%), Space: 11 MB (29.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,37 +1,31 @@
 class Solution {
-public:    
+public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Naive Approach
-        // for(int i=0;i<nums.size();i++){
-        //     int j=0;
-        //     while(j<nums.size()){
-        //         if(i==j){
-        //             j++;
-        //             continue;
-        //         }
-        //         else{
-        //             if((target-nums[i])==nums[j]){
-        //                 return {i,j};
-        //             }
-        //         }
-        //         j++;
-        //     }
-        // }
-        // return {};
+    /*
+    two solutions : 
+    
+    brute force
+    two loops to move over each element and find the sum and check it with target
+    
+    two pointer approach:
+    have to be sorted first and then to increase sum value add from start and to decrease sum remove from the start through pointers
+    
+    optimised approach but not space optimised
+    hash map approach
+    store the keys in the hashmap and check if diff exists in hashmap
+    */ 
+    int n=nums.size();
+    unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            // mp[nums[i]]=i;
+            if(mp.find(target-nums[i])!=mp.end())
+            {   cout<<"yes";
+                return {mp[target-nums[i]],i};
+            } 
+            mp[nums[i]]=i;
+                
+        }
         
-        //Better Complexity Approach
-        unordered_map<int,int>imp;
-        // vector<int> temp; no need as vector can be returned using { }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(imp.find(target-nums.at(i))!=imp.end())
-                   return{imp[target-nums.at(i)],i};
-                    // here imp[target-nums.at(i)] is the second index and i is the current one.
-            
-            imp[nums.at(i)]=i;
-        
-        } 
         return {};
-//                empty vector
     }
 };
