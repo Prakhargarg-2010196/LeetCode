75c79badb71e15f546e20bf7f143e6e006318266
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-02-05 00:03:43 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (54.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stk;
+        for(auto&it:s){
+            if(it=='('||it=='{'||it=='[')
+                stk.push(it);
+            else{
+                   if(!stk.empty()&&(it==')'&&stk.top()=='('||it=='}'&&stk.top()=='{'||it==']'&&stk.top()=='['))
+            {
+                stk.pop();
+                
+            }
+            else return false;
+            }       
+        }
+        return stk.empty();
+    }
+};
