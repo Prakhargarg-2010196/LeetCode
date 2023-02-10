cea749671f2914508b43895266371be81332ee6d
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-02-10 18:42:51 +0530

    Time: 6 ms (45.14%), Space: 6.5 MB (95.17%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int flag=0;
-        stack<char>st;
+        // stack<char>st;
+        // string res;
+        // for(auto&it:s){
+        //     if(it=='(')
+        //     {   if(st.size()>0)// append '(' when 1 more '(' is already present
+        //         {   
+        //             res+=it;  
+        //         }
+        //         st.push(it);
+        //     }
+        //     else {
+        //         if(st.size()>1) // as when two '(' are present then only we need to append ')' to result
+        //             res+=it;
+        //         st.pop();
+        //     }
+        
+        
+        // Without stack solution O(1) space
+        // take a count for checking if it is the first (is >0) for '('and last element (is >1) for ')' 
+        int count=0;
         string res;
         for(auto&it:s){
-            if(it=='(')
-            {   if(st.size()>0)// append '(' when 1 more '(' is already present
-                {   
-                    res+=it;  
+            if(it=='('){
+                if(count>0){
+                    res+=it;
                 }
-                st.push(it);
+                count++;//increase the count value
             }
             else {
-                if(st.size()>1) // as when two '(' are present then only we need to append ')' to result
+                if(count>1){
                     res+=it;
-                st.pop();
+                }
+                count--;
             }
         }
+        
+        
+         
         return res;
     }
 };
