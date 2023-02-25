4dc96326e659726626ff96771f9a6a047d0555a4
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-02-25 12:12:26 +0530

    Time: 145 ms (8.93%), Space: 108.6 MB (5.29%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -6,16 +6,19 @@ public:
         int startIndex=0;
         for(int i=0;i<n;i++){
              
-            // store the gain from the current index
+            // store the gain from current station i.e the distance or fuel it gains for the next station
             int gain=gas[i]-cost[i];
             // inc the balance by the gain
             balance+=gain;
-            if(balance<0)
+            /*if the balance becomes -ve then we continue from the next station leaving all the station in the back as each station's gain decrease the -ve value and if we start by a next station then it would only mean to increase the -ve value so skip the stations previous to the current station when the value turn to be -ve and also store the loss as it would help us in the forthcoming stations(simulating the circular value) also reset the balance as we would start a new journey here*/
+            
+            if(balance<0) 
             {  startIndex=i+1;
                loss+=balance;
                balance=0;// refresh balance when the balance is found -ve
             }
         }
+        
         if(balance+loss>=0){
             return startIndex;
         }
