84d06cf64f59e0c3890d78c5f0a819d5e1c613c6
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-03-04 11:42:38 +0530

    Time: 5 ms (30.89%), Space: 7.1 MB (79.09%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,16 +11,15 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL)
-        {
-            slow=slow->next;
-            fast=fast->next->next;
-            
-        }
+     // OA: find the middle using slow fast pointer
+        // here the after every two nodes the middle value changes 
+        // for example for 1 middle=1 for 2 middle=2 for 3 middle =2 but for 4 middle=3 so we can see that two nodes must be there before the middle therefore movement of the fast node or the count node must be twice as of the answer or slow node. 
+     ListNode* slow=head;
+     ListNode* fast=head;
+     while(fast&&fast->next){// two nodes must be present after the middle node
+         slow=slow->next;
+         fast=fast->next->next;// movement of pointer twice 
+     }
         return slow;
-        
-        
     }
 };
