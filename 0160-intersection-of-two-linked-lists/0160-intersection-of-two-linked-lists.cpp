32df992c68da0fe60622c20d519aca6b231c0c0a
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-03-11 12:58:29 +0530

    Time: 42 ms (92.18%), Space: 14.6 MB (35.79%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,22 +11,22 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         // Brute Force 
         // store the nodes in hashmap (as finding takes O(1)) and then compare.
-        map<ListNode*,bool>mp;
-        ListNode* tr1=headA;
-        ListNode* tr2=headB;
-        while(tr1){
-            // store in map
-            mp[tr1]=true;
-            tr1=tr1->next;
-        }
-        while(tr2){
-            // check if it exists in map
-            if(mp[tr2]==true){
-                return tr2;                
-            }
-            tr2=tr2->next;
-        }
-        return NULL;
+//         map<ListNode*,bool>mp;
+//         ListNode* tr1=headA;
+//         ListNode* tr2=headB;
+//         while(tr1){
+//             // store in map
+//             mp[tr1]=true;
+//             tr1=tr1->next;
+//         }
+//         while(tr2){
+//             // check if it exists in map
+//             if(mp[tr2]==true){
+//                 return tr2;                
+//             }
+//             tr2=tr2->next;
+//         }
+//         return NULL;
         
         
         // Optimised
@@ -34,15 +34,44 @@ public:
         /*
             We need to find the node with same addr in both lists
             Let 
-            x: dist from start of l1 to IP
-            y: dist from start of l2 to IP
+            x: dist from start of headA to IP
+            y: dist from start of headB to IP
             g: length from IP to end.
-            
-            
-        
-        
-        
+            l1: x+g
+            l2: y+g
+            if x==y => tr1 and tr2 will intersect at same time
+            but when x!=y dist has to be made same 
+            by adding the equivalent dist to each list when any of them finshes traversal
+            i.e l1 after complete traversal becomes
+                l1: x+g+y  (tr1 goes null=> initialise with headB)
+                l2: y+g+x  (tr2 goes null=> initialise with headA)
+                this way both dist becomes same and they again intersect at same moment
+            This will work in case when lists don't intersect too.
         */
+        ListNode* tr1=headA;
+        ListNode* tr2=headB;
+        if(!tr1||!tr2){
+            return NULL;
+        }
+       
+            while(tr1!=tr2){
+            
+           // move until they meet 
+           
+            if(!tr1){
+                tr1=headB;
+            }
+            else 
+                tr1=tr1->next;
+            if(!tr2){
+                tr2=headA;
+            }
+           else 
+                tr2=tr2->next;
+                
+        }
+            
+        return tr1;
         
     }
 };
