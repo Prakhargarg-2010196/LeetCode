1bd127c132cbd1c15ef923a5e29b330ab20308ad
Prakhar Garg <23prakhargarg2002@gmail.com>
2023-04-04 19:36:19 +0530

    Time: 8 ms (33.23%), Space: 16.8 MB (7.82%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -9,50 +9,74 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    bool helper(TreeNode* ls,TreeNode* rs){
-        // if both nodes are null then tree is symm
-        if(ls==NULL&& rs==NULL )
-            return true;
-        // if one node is null and other has value then tree is not symm
-        if(ls!=NULL && rs==NULL)
-            return false;
-        if(rs!=NULL&& ls==NULL)
-            return false;
-        // if both has value check
-        if(rs->val == ls->val)
-            // check if the left node of left subtree == right node of right subtree
-            // also check if the right node of left subtree == left node of right subtree
-            return helper(ls->left,rs->right)&&helper(ls->right,rs->left);
-        
-      return false;
-    }
-    bool isSymmetric(TreeNode* root) {
-        
-        // first step write base condition 
-        // if there is no root no need to perform recursion 
-        if(root == NULL )
-            return true;
-        // use a helper function to return the result of recursion over left and right subtree
-        return helper(root->left,root->right);
-    }
-};
-
-
-
-
-
-
-
-
-
-
-// iterative solution
 // class Solution {
 // public:
+//     bool helper(TreeNode* ls,TreeNode* rs){
+//         // if both nodes are null then tree is symm
+//         if(ls==NULL&& rs==NULL )
+//             return true;
+//         // if one node is null and other has value then tree is not symm
+//         if(ls!=NULL && rs==NULL)
+//             return false;
+//         if(rs!=NULL&& ls==NULL)
+//             return false;
+//         // if both has value check
+//         if(rs->val == ls->val)
+//             // check if the left node of left subtree == right node of right subtree
+//             // also check if the right node of left subtree == left node of right subtree
+//             return helper(ls->left,rs->right)&&helper(ls->right,rs->left);
+        
+//       return false;
+//     }
 //     bool isSymmetric(TreeNode* root) {
         
-//     
+//         // first step write base condition 
+//         // if there is no root no need to perform recursion 
+//         if(root == NULL )
+//             return true;
+//         // use a helper function to return the result of recursion over left and right subtree
+//         return helper(root->left,root->right);
 //     }
 // };
+
+// iterative solution
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        // in iterative as there is no implicit stack we need to use other data structures 
+        // for memory 
+        queue<TreeNode*>ls;
+        queue<TreeNode*>rs;
+        ls.push(root->left);
+        rs.push(root->right);
+    
+        while(!ls.empty()&&!rs.empty()){
+            TreeNode* l=ls.front();
+            ls.pop();
+            TreeNode* r=rs.front();
+            rs.pop();
+            if(l&&r&&l->val!=r->val)
+                return false;
+            if(l&&r==NULL)
+                return false;
+            if(l==NULL&&r)
+                return false;
+            if(l&&r&&l->val==r->val){
+                if(l){
+                    ls.push(l->left);
+                    ls.push(l->right);
+                }
+                if(r){
+                    rs.push(r->right);
+                    rs.push(r->left);
+                }
+            }
+           
+            
+        }
+         if(ls.empty()&&rs.empty())
+                return true;
+        else   
+            return false;
+    }
+};
